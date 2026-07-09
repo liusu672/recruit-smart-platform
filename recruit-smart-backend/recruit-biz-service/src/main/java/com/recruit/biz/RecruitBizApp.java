375bf6162db576f497f5d0d99f4a68ce7f15b44e
liusu672 <2633066533@qq.com>
2026-07-09 11:58:50 +0800

    init recruit smart platform backend framework

---
+++ recruit-smart-backend/recruit-biz-service/src/main/java/com/recruit/biz/RecruitBizApp.java
@@ -0,0 +1,13 @@
+package com.recruit.biz;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.recruit.biz.mapper")
+@SpringBootApplication
+public class RecruitBizApp {
+    public static void main(String[] args){
+        SpringApplication.run(RecruitBizApp.class,args);
+    }
+}
